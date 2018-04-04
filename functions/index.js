2e23d4b7c2c28a701e841092bfba2b4a8f11d76c
Danny Voce <davjr96@gmail.com>
2018-04-04 14:47:12 -0400

    Start functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./deweybike-firebase-adminsdk-jjxwg-d9b8016d17.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://deweybike.firebaseio.com"
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+exports.addReading = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const id = req.query.id;
+  const measurement = req.query.measurement;
+
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  return admin
+    .database()
+    .ref("/reading")
+    .push({ id: id, measurement: measurement })
+    .then(snapshot => {
+      // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+      return res.redirect(303, snapshot.ref.toString());
+    });
+});
